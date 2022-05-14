d88002b92df0bf92a7c56d9ed7f3ea3a0890c4d3
kevinstring <kevingabrielmdn@gmail.com>
2022-05-14 02:28:35 -0600

    retos

--- js/players.js
+++ js/players.js
@@ -1,5 +1,15 @@
-var user = [
-    "kevin",
-    "alejandro",
-    "gabriel"
-];
+
+var user = [];
+
+function addUser(){
+
+
+var userName = document.getElementById("userName").value;
+var lista= document.getElementById("lista");
+lista.innerHTML+= "<p>"+userName+"</p>"
+user.push(userName);
+document.getElementById("userName").value="";
+console.log(user);  
+
+
+}
